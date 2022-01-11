c0d373d8e2dbab85a32229c1cd6b81d3fb0ec0c8
d4ng3r228 <dangersof228@gmail.com>
2022-01-11 20:02:29 +0200

    testCommit

---
+++ src/main/java/com/poltavskiy/customer_accounting_system/CustomerAccountingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.poltavskiy.customer_accounting_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAccountingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAccountingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poltavskiy/customer_accounting_system/CustomerAccountingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poltavskiy.customer_accounting_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAccountingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
